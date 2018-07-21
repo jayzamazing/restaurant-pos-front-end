f9cf1b37a077b3542c72394e8c9e52cc33e98997
Adrian J Lopez <206582563>
2018-07-21 14:14:42 -0400

    Show logout button if logged in, added loggedin state to check if user is auth.

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import './header-bar.css'
+import {connect} from 'react-redux';
+import './header-bar.css';
+import {setCurrentUser, setAuthToken} from '../actions/auth';
+import {clearAuthToken} from '../local-storage';
+import {Link} from 'react-router-dom';
 
 export class HeaderBar extends React.Component {
+  //logout user and invalidate session
+  logOut() {
+      this.props.dispatch(setCurrentUser(null));
+      this.props.dispatch(setAuthToken(null));
+      clearAuthToken();
+  }
   render() {
+    console.log(this.props.loggedIn)
+    let logout;
+    if (this.props.loggedIn) {
+      logout = (
+        <div className="logout-btn">
+          <button className="btn logout-btn" onClick={() => this.logOut()}>Log out</button>
+        </div>
+      );
+    }
     return (
       <header className="container-fluid">
         <div className="jumbotron">
           <h1>Restaurant POS</h1>
+          {logout}
         </div>
       </header>
     );
   }
 }
-export default HeaderBar;
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+export default connect(mapStateToProps)(HeaderBar);
