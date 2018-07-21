02a54d9238c7c0b9468d43dc545adccd0bad1827
Adrian J Lopez <206582563>
2018-07-21 13:08:56 -0400

    Add size to form, add placeholders for username and password fields.

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -4,6 +4,7 @@ import Input from './input';
 import {login} from '../actions/auth';
 import {required, nonEmpty} from '../validators';
 import {connect} from 'react-redux';
+import './login-form.css';
 
 export class LoginForm extends React.Component {
     componentWillMount() {
@@ -19,18 +20,20 @@ export class LoginForm extends React.Component {
             error = <div className="form-error">{this.props.error}</div>;
         }
         return (
-            <form className="login-form" onSubmit={this.props.handleSubmit(values =>
+            <form className="login-form col-xs-12 col-sm-8 col-md-8 col-md-8 center_div ng-pristine ng-valid" onSubmit={this.props.handleSubmit(values =>
                   this.props.login(values.email, values.password, values.store))}>
                 {error}
                 <Field
-                  label="Username"
+                  label="username"
+                  placeholder="Please enter your username"
                   component={Input}
                   type="text"
                   name="username"
                   id="username"
                   validate={[required, nonEmpty]} inputClass="input-field form-control"/>
                 <Field
-                  label="Password"
+                  label="password"
+                  placeholder="Please enter your password"
                   component={Input}
                   type="password"
                   name="password"
