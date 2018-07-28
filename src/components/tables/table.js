fbfa4bef53e26020309565f43d21af0784c89f63
Adrian J Lopez <206582563>
2018-07-28 01:07:46 -0400

    Rework of table to be a component, added begindragm enddrag, collect functions and added test for it.

--- src/components/tables/table.js
+++ src/components/tables/table.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {DragSource} from 'react-dnd'
 import table_4 from '../img/brown4persontable.png';
 import table_6 from '../img/brown6persontable.png';
+import {Types} from '../../constants';
 import './table.css';
 
-export default function Table(props) {
-  let tableImg = function(table) {
-    if (table == 'table_4')
+export class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tablePosition: []
+    }
+  }
+  tableImg = function(table) {
+    if (table === 'table_4')
       return table_4;
     else
       return table_6
   }
-  return (
-    <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
-      <figure>
-        <img src={tableImg()} alt={props.alt} className="img-thumbnail tableImg" />
-        <figcaption className={props.figClass}>{props.figNumber}</figcaption>
-      </figure>
-    </div>
-  );
+  render () {
+    const {connectDragSource, src } = this.props
+    return connectDragSource (
+      <div>
+        <figure>
+          <img src={this.tableImg(src)} alt={this.props.alt} className="img-thumbnail tableImg" />
+          <figcaption className={this.props.figClass}>{this.props.figNumber}</figcaption>
+        </figure>
+      </div>
+    );
+  }
 }
+const tableSource = {
+  beginDrag(props) {
+    return {};
+  },
+  endDrag(props) {
+     console.log('end drag');
+    return {};
+  }
+}
+function collect(connect, monitor) {
+ return {
+   connectDragSource: connect.dragSource(),
+   isDragging: monitor.isDragging()
+ }
+}
+Table.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired
+};
+export default DragSource(Types.TABLE, tableSource, collect)(Table);
