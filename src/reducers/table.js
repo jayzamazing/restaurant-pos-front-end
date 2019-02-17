77b6bbfc30b7d715ea7154cf1d4f01e8d4ad815e
Adrian J Lopez <206582563>
2019-02-16 21:13:42 -0500

    Changed initialRepositoryState to be two dimensional array, Added updateTable and removeTable functions to table reducer.

--- src/reducers/table.js
+++ src/reducers/table.js
@@ -1,15 +1,36 @@
-import {MOVE_TABLE} from '../constants';
+import {MOVE_TABLE, REMOVE_TABLE} from '../constants';
 
-const initialRepositoryState = {position: {tx: 0, ty: 0}};
+const initialRepositoryState = [[]]
+
+const updateTable = (state, tableNumber, position) => {
+  //make shallow copy of state array
+  let tempState = state.slice();
+  if (!tempState[position.tx]) {
+    tempState[position.tx] = [];
+  }
+  if (!tempState[position.tx][position.ty]) {
+    tempState[position.tx][position.ty] = [];
+  }
+  tempState[position.tx][position.ty] = {tableNumber: tableNumber};
+  return tempState;
+}
+const removeTable = (state, position) => {
+  if (!state[position.tx] && !state[position.tx][position.ty]) {
+    return state;
+  }
+  //make shallow copy of state array
+  let tempState = state.slice();
+  tempState[position.tx][position.ty] = {};
+  return tempState;
+}
 
 export default function reducer(state = initialRepositoryState, action) {
-  const {position} = action;
+  const {position, tableNumber} = action;
   switch (action.type) {
     case MOVE_TABLE:
-    return {
-      ...state,
-      position
-    }
+      return updateTable(state, tableNumber, position);
+    case REMOVE_TABLE:
+      return removeTable(state, position);
     default:
       return state
   }
